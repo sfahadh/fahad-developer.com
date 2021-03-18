0557e51cc9d59920bdf7f3b3f1a6914849631f81
sfahadh <sfahadh14@gmail.com>
2021-03-18 19:31:14 -0400

    installed sass

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,8 +16,10 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -28,6 +32,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
