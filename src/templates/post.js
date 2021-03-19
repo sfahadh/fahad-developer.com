b94510a59d8ea75c566408b7567c13fd802c2aa0
sfahadh <sfahadh14@gmail.com>
2021-03-19 15:52:10 -0400

    queried posts by slug in post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,11 +1,30 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-const Post = () => (
-    <Layout>
-        This is a post template
-    </Layout>
-)
+const Post = ({ data }) => {
+    const post = data.contentfulBlogPost;
+
+    return (
+        <Layout>
+            <h1>{ post.title }</h1>
+            <h6>{post.published}</h6>
+            <p>{ post.body.raw }</p>
+        </Layout>
+    )
+}
 
 export default Post;
+
+export const pageQuery = graphql`
+    query($slug: String!) {
+        contentfulBlogPost(slug: { eq: $slug }) {
+            title
+            published(formatString: "MMMM Do, YYYY")
+            body {
+                raw
+            }
+        }
+    }
+`
 
