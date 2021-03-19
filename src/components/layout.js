bffa01b2f7c4b5f7b4b083d5d1628b9fc30ea8f7
sfahadh <sfahadh14@gmail.com>
2021-03-19 18:09:26 -0400

    landing page take full width of screen

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,24 +27,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout-container">
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
     </div>
   )
 }
