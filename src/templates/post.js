5aad8ad6089e511836b9cef08b3937cd932d0d34
sfahadh <sfahadh14@gmail.com>
2021-03-19 15:05:00 -0400

    created a post template

---
+++ src/templates/post.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const Post = () => (
+    <Layout>
+        This is a post template
+    </Layout>
+)
+
+export default Post;
+
