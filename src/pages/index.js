fe0b2678de59e563a08c5245306e16e70d97d142
sfahadh <sfahadh14@gmail.com>
2021-03-18 22:51:03 -0400

    queried blogs from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = props => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -27,3 +28,29 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+	query {
+    allContentfulBlogPost(sort: {fields: published, order: DESC}) {
+      edges {
+        node {
+          title
+          slug
+          tags
+          published(formatString: "MMMM Do, YYYY")
+
+          synopsis {
+            synopsis
+          }
+
+          thumbnail {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+	}
+`
+
