7be5c2145f25573942be866d3fe08e6dbbd5019a
sfahadh <sfahadh14@gmail.com>
2021-03-20 11:16:21 -0400

    installed react icons

--- src/components/main/about.js
+++ src/components/main/about.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import { VscTrash } from "react-icons/vsc";
-// import { GiMuscleFat } from "react-icons/gi";
-// import dev from "../../../assets/images/dev.svg";
+import { VscTrash } from "react-icons/vsc";
+import { GiMuscleFat } from "react-icons/gi";
+import dev from "../../images/dev.svg";
 import "../../styles/main/about.scss";
 
 const About = () => {
@@ -10,19 +10,14 @@ const About = () => {
             <div className="container-sm aboutMe">
                 <div className="details">
                     <h1 className="mb-3">about me</h1>
-                    <h3 className="mb-3">An engineer who likes to eat, sleep & lift
-                    {/* <GiMuscleFat /> */}
-                    </h3>
-                    <p>Transitioned from a career as a Personal Trainer with an 
-                    original goal of becoming a Physical Therapist. Treat me to lunch (coffee is
-                        {/* <VscTrash /> */}
-                        ) and I'll
-                        tell you more
+                    <h3 className="mb-3">An engineer who likes to eat, sleep & lift <GiMuscleFat /></h3>
+                    <p>Transitioned from a career as a Personal Trainer with an original goal of becoming
+                        a Physical Therapist. Treat me to lunch (coffee is<VscTrash />) and I'll tell you more
                     </p>
                 </div>
                 
                 <div className="thumbnail">
-                    {/* <img src={dev} alt="I’m Fahad, buy me lunch!" /> */}
+                    <img src={dev} alt="I’m Fahad, buy me lunch!" />
                 </div>
             </div>
         </section>
