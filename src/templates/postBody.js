e0fcf0eede58f92d926976a11de0ecb9d0361eb7
sfahadh <sfahadh14@gmail.com>
2021-03-22 23:15:36 -0400

    rendered paragraph text from blog

--- src/templates/postBody.js
+++ src/templates/postBody.js
@@ -1,8 +1,11 @@
-import React from "react"
-import { BLOCKS, MARKS } from "@contentful/rich-text-types"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import React from "react";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 
 const PostBody = ({ content }) => {
+    const Bold = ({ children }) => <p className="bold">{ children }</p>;
+    const Text = ({ children }) => <p className="align-center">{children}</p>;
+    
     const document = {
         nodeType: 'document',
         content: [{
@@ -22,8 +25,20 @@ const PostBody = ({ content }) => {
         }]
     };
 
+    const options = {
+        renderMark: {
+            [MARKS.BOLD]: text => <Bold>{ text }</Bold>,
+        },
+
+        renderNode: {
+            [BLOCKS.PARAGRAPH]: (node, children) => <Text>{ children }</Text>,
+        },
+
+        renderText: text => text.replace('!', '?'),
+    };
+
     return (
-        <article className="blog-body">{ documentToReactComponents(document) }</article>
+        <article className="blog-body">{ renderRichText(content, options) }</article>
     )
 }
 
