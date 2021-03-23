9f71b4e261e475eec344ae114523e0c4f86ac699
sfahadh <sfahadh14@gmail.com>
2021-03-23 10:58:59 -0400

    deleted document variable and styled header tags

--- src/templates/postBody.js
+++ src/templates/postBody.js
@@ -5,25 +5,6 @@ import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 const PostBody = ({ content }) => {
     const Bold = ({ children }) => <p className="bold">{ children }</p>;
     const Text = ({ children }) => <p className="align-center">{ children }</p>;
-    
-    const document = {
-        nodeType: 'document',
-        content: [{
-            nodeType: 'paragraph',
-            content: [
-                {
-                    nodeType: 'text',
-                    value: 'Hello',
-                    marks: [{ type: 'bold' }],
-                },
-                {
-                    nodeType: 'text',
-                    value: ' world!',
-                    marks: [{ type: 'italic' }],
-                }
-            ],
-        }]
-    };
 
     const options = {
         renderMark: {
@@ -34,6 +15,7 @@ const PostBody = ({ content }) => {
             [BLOCKS.HEADING_2]: (node, children) => <h2>{ children }</h2>,
             [BLOCKS.HEADING_4]: (node, children) => <h4>{ children }</h4>,
             [BLOCKS.PARAGRAPH]: (node, children) => <Text>{ children }</Text>,
+            // [BLOCKS.EMBEDDED_ASSET]: node => <code>{ JSON.stringify(node, null, 2) }</code>,
         }
     };
 
