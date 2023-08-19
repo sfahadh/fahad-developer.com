25a6c0e820ea0ab8e45510f07332ca1fca6e6fb9
Fahad Hussain <sfahadh14@gmail.com>
2023-08-19 10:21:10 -0400

    added styles

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,9 +6,19 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `fahad-developer.com`,
-    siteUrl: `https://www.fahad-developer.com`
-  },
+		title: `Fahad Hussain`,
+		description: `A portfolio of my work and blogs I've written using Gatsby, Contentful, and Sass. 
+			It displays completed Javascript projects and blogs written about Gatsby, Javascript, and Algorithms & Data Structures`,
+		author: `Fahad Hussain`,
+		siteUrl: 'https://www.fahad-developer.com',
+		socialLinks: {
+			linkedIn: `https://www.linkedin.com/in/fahad-hussain23/`,
+			github: `https://github.com/sfahadh`
+		},
+		email: `sfahadh14@gmail.com`,
+		address: `New York, NY`,
+		keywords: ["Gatsby", "Sass", "Contentful", "Headless CMS", "Netlify", "Portfolio", "Blog"]
+	},
 
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -21,7 +31,12 @@ const config: GatsbyConfig = {
       "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
       "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+  }, 
+  "gatsby-plugin-image", 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp",
+  "gatsby-plugin-sass"
+  ]
 };
 
 export default config;
