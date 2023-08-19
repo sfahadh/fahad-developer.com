60e7ebb4cfe953cf06aa5b36ee97f28e46f252e1
Fahad Hussain <sfahadh14@gmail.com>
2023-08-19 00:03:19 -0400

    installed env

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,25 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `fahad-developer.com`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.fahad-developer.com`
   },
 
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  
+
   graphqlTypegen: true,
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "",
-      "spaceId": ""
+      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+      "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
 };
